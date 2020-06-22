2162cb328a16c10fcb57d3d44ae6634d59ca7616
liuaaahui <50660887+liuaaahui@users.noreply.github.com>
2020-06-22 19:42:53 +0800

    Update dml.sql

--- dml.sql
+++ dml.sql
@@ -1,8 +1,8 @@
 -- Insert record
-INSERT INTO student(id,name,age,sex) VALUES ('003','Penny',22,'female');
+INSERT INTO student(id,name,age,sex) VALUES ("003","Penny",22,"female");
 -- Revise record
-UPDATE student SET age=20 WHERE id='003';
+UPDATE student SET age=20 WHERE id="003";
 --  Delete record
-DELETE FROM student WHERE name='Penny';
+DELETE FROM student WHERE name="Penny";
 -- Search record
 SELECT * FROM student WHERE age=18;
